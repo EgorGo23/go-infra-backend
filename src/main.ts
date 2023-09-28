d3df2a65c882dac1aa8e242323d5fbed6f3bb565
egor.go23 <egor.go2303@gmail.com>
2023-09-28 18:26:16 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost', 'http://localhost:4552'],
+  });
+
   app.setGlobalPrefix('api');
 
   const config = await app.get(ConfigService);
