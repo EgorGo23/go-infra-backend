6a90bff4f4facbb4be6a25ac76d23217dbeeb9fd
Egor <egor.golenok@softswiss.com>
2023-09-29 15:00:28 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: "*",
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type'
   });
 
   app.setGlobalPrefix('api');
